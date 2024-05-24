b8ade09820766cab9cdcd93987eb22565a81798e
Gitphin <105572640+Gitphin@users.noreply.github.com>
2024-05-24 13:27:28 -0500

    First upload (work in progress)
    Implemented Treesitter, Telescope, Kanagawa color scheme, Neo tree, LSP support, and lualine

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,10 @@
+-- Language / parsing support
+return {"nvim-treesitter/nvim-treesitter", 
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({ensure_installed = {"lua", "python", "vim", "c", "rust", "javascript"},
+    highlight = {enable = true}, 
+    indent = {enable = true}})
+  end
+}
---
+++ nvim/vops.lua
@@ -0,0 +1,8 @@
+-- Indent spacing
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+-- Leader key set to SPACE
+vim.g.mapleader = " "
+
